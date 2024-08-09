0efd698014d4cd8ecf3d2b6f0958a96d1b10c3f9
matsvei.lishai <matsvei.lishai@softswiss.com>
2024-08-09 22:31:05 +0200

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSameLine: true,
+  semi: true,
+  arrowParens: 'avoid',
+  endOfLine: "auto",
+};
